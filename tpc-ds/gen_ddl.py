e15496849b3cf5f0f9abdd4cac92ffbe09ebd899
Mohammad Mohsen <m.m.asemanmanzar@gmail.com>
2019-08-25 00:56:22 +0430

    ignore dbgen_version table!

--- tpc-ds/gen_ddl.py
+++ tpc-ds/gen_ddl.py
@@ -21,10 +21,12 @@ drop table if exists {table_name}_text;
 
 """
 data_path = os.environ['data_path']
+ignored_tables = ["dbgen_version"]
 
 with open(out_file_path, 'w') as out_file, open(in_file_path, 'r', encoding='utf-8') as in_file:
     matches = re.finditer(regex, in_file.read(), re.MULTILINE)
 
     for match in matches:
         groups = list(match.groups())
-        out_file.write(template.format(data_path=data_path, table_name=groups[0], table_columns=groups[1]))
+        if groups[0] not in ignored_tables:
+            out_file.write(template.format(data_path=data_path, table_name=groups[0], table_columns=groups[1]))
